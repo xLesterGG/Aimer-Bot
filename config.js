e93b3d6159a12e0715876fd78c64acf00fb4920e
Lester <lesgoh@hotmail.com>
2018-08-21 22:22:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: 'eUbSiSgxL68ohWB2OfADdeBgh',
-  consumer_secret: '2OtLRjbHYQ0DF8dRxZH6KxP46Xdhi0B5ivCJSHl5eHN3gTqCFn',
-  access_token: '941527549804396544-23lvuaAm0dKun03uUxt6f5KLfkxIvdp',
-  access_token_secret: 'mw71Hckk8iboc1zs4hEhp6EqOzhOZyo9gwv4PduTLdjiJ'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
 }
